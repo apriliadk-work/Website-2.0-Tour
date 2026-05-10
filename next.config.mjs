4cb5a64e8c1bd8a4e5ccc5a8837ce7e1a1f70497
apriliadk-work <apriliadkwork@gmail.com>
2026-05-10 23:47:12 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
+  poweredByHeader: false,
+  compress: true,
+};
+
+export default nextConfig;
